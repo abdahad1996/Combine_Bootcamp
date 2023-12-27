5c8c4104558854fd8e00efdb2b798b4121444063
abdul ahad <a.ahad@geomobile.de>
2023-12-27 22:46:09 +0100

    - used published property wrapper as a publisher

--- Publisher/Publisher/ContentView.swift
+++ Publisher/Publisher/ContentView.swift
@@ -7,13 +7,30 @@
 
 import SwiftUI
 
+class ViewModel:ObservableObject{
+    @Published var name:String = ""
+    @Published var validation:String = ""
+    
+    init() {
+        $name.map{
+            print("name is \(self.name)")
+            print("value in pipeline is \($0)")
+
+           return $0.isEmpty ? "its empty":"filled"
+        }.assign(to:&$validation)
+    }
+}
 struct ContentView: View {
+   @StateObject var viewModel = ViewModel()
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Hello, world! \(viewModel.name)")
+            TextField("name", text: $viewModel.name).textFieldStyle(RoundedBorderTextFieldStyle())
+            Text(viewModel.validation)
+            
         }
         .padding()
     }

0730cca7064cb2d8e1f15aad92529a381d369f87
abdul ahad <a.ahad@geomobile.de>
2023-12-27 21:33:53 +0100

    - craete a repo to store combine working examples

---
+++ Publisher/Publisher/PublisherApp.swift
@@ -0,0 +1,17 @@
+//
+//  PublisherApp.swift
+//  Publisher
+//
+//  Created by Abdul Ahad on 27.12.23.
+//
+
+import SwiftUI
+
+@main
+struct PublisherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

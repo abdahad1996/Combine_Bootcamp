c42927e3a3f2691254a0319a8bbae7dec8ecf6e8
abdul ahad <a.ahad@geomobile.de>
2024-01-01 18:08:28 +0100

    - added just

---
+++ Just/Just/ContentView.swift
@@ -0,0 +1,45 @@
+//
+//  ContentView.swift
+//  Just
+//
+//  Created by Abdul Ahad on 01.01.24.
+//
+
+import SwiftUI
+import Combine
+/*“Using the Just publisher can turn any variable into a publisher. It will take any value you have and send it through a pipeline that you attach to it one time and then finish (stop) the pipeline.”
+*/
+//“This means you can attach                               pipelines to any property or value.”
+
+class justViewModel:ObservableObject {
+    @Published var firstNameFromArray = ""
+    
+    func fetch(){
+        let data =  ["abdul","kareem","abdullah"]
+        
+        Just(data[0]).map{$0.uppercased()}
+            .print()
+            .assign(to: &$firstNameFromArray)
+    }
+}
+struct ContentView: View {
+    @StateObject var vm = justViewModel()
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, \(vm.firstNameFromArray)!")
+            
+            Button("tap here to get the name"){
+                vm.fetch()
+            }
+        }
+        .padding()
+        
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Just/Just/JustApp.swift
@@ -0,0 +1,17 @@
+//
+//  JustApp.swift
+//  Just
+//
+//  Created by Abdul Ahad on 01.01.24.
+//
+
+import SwiftUI
+
+@main
+struct JustApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
